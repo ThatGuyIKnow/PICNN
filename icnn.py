c37c4af9793302b8f4016166b3fded81317633d6
Oliver Harold <oliverhsj@hotmail.dk>
2024-03-15 20:51:51 +0700

    JIT

--- icnn.py
+++ icnn.py
@@ -1,25 +1,21 @@
-
-import torch.nn as nn
 import torch
-from torch.autograd import Variable
+import torch.nn as nn
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
+from torch.jit import ScriptModule, script_method
+from torch import Tensor
+from typing import Tuple
 
-
-class ICNN(nn.Module):
-    def __init__(self, M, N, kernel_size, stride, padding, classifier, norm_template=1, device=None):
+class ICNN(ScriptModule):
+    def __init__(self, M: int, N: int, kernel_size: Tuple[int, int], stride: Tuple[int, int], padding: Tuple[int, int], classifier: nn.Module, norm_template: float = 1, device: torch.device = None):
         super(ICNN, self).__init__()
-        self.add_conv = nn.Conv2d(in_channels=M, out_channels=N,
-                                  kernel_size=kernel_size, stride=stride, padding=padding)
+        self.add_conv = nn.Conv2d(in_channels=M, out_channels=N, kernel_size=kernel_size, stride=stride, padding=padding)
         self.classifier = classifier
-        # create templates for all filters
         self.out_size = 9
 
         mus = torch.FloatTensor([[i, j] for i in range(self.out_size) for j in range(self.out_size)])
         templates = torch.zeros(mus.size(0), self.out_size, self.out_size)
 
         n_square = self.out_size * self.out_size
-
         tau = 0.5 / n_square
         alpha = n_square / (1 + n_square)
         beta = 4
@@ -32,27 +28,24 @@ class ICNN(nn.Module):
                         out = tau * torch.clamp(1 - beta * norm / self.out_size, min=-1)
                         templates[k, i, j] = float(out)
 
-        self.templates_f = Variable(templates, requires_grad=False).to(device)
+        self.templates_f = templates.requires_grad_(False).to(device)
         neg_template = -tau * torch.ones(1, self.out_size, self.out_size)
         templates = torch.cat([templates, neg_template], 0)
-        self.templates_b = Variable(templates, requires_grad=False).to(device)
+        self.templates_b = templates.requires_grad_(False).to(device)
 
         p_T = [alpha / n_square for _ in range(n_square)]
         p_T.append(1 - alpha)
-        self.p_T = Variable(torch.FloatTensor(p_T), requires_grad=False).to(device)
+        self.p_T = torch.FloatTensor(p_T).requires_grad_(False).to(device)
 
-    def get_masked_output(self, x):
-        # choose template that maximize activation and return x_masked
+    @script_method
+    def get_masked_output(self, x: Tensor) -> Tuple[Tensor, Tensor]:
         indices = F.max_pool2d(x, self.out_size, return_indices=True)[1].squeeze()
         selected_templates = torch.stack([self.templates_f[i] for i in indices], 0)
         x_masked = F.relu(x * selected_templates)
         return x_masked, selected_templates
 
-    def compute_local_loss(self, x):
-        # x = x.permute(1, 0, 2, 3)
-        # exp_tr_x_T = (x[:, :, None, :, :] * self.templates_b[None, None, :, :, :]).sum(-1).sum(-1).exp()
-        # Z_T = exp_tr_x_T.sum(1, keepdim=True)
-        # p_x_T = exp_tr_x_T / Z_T
+    @script_method
+    def compute_local_loss(self, x: Tensor) -> Tensor:
         tr_x_T = torch.einsum('bcwh,twh->cbt', x, self.templates_b)
         p_x_T = F.softmax(tr_x_T, dim=1)
 
@@ -61,16 +54,11 @@ class ICNN(nn.Module):
         loss = - (self.p_T[None, :] * p_x_T_log).sum(-1)
         return loss
 
-    def forward(self, x, train=True):
+    @script_method
+    def forward(self, x: Tensor, train: bool = True) -> Tuple[Tensor, Tensor, Tensor]:
         x1, _ = self.get_masked_output(x)
         x = self.add_conv(x1)
-        # x2, _ = self.get_masked_output(x)
-        
-        
         x = self.classifier(x)
 
-        # compute local loss:
         loss_1 = self.compute_local_loss(x1)
-        # loss_2 = self.compute_local_loss(x2)
-
         return x, x1, loss_1
